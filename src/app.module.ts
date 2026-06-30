f8484e1201a397bdbe4cdda2fa11eb91b4b69379
Ahmad MKH <apolonsoft@gmail.com>
2026-06-30 18:42:47 +0330

    Add required npm packages

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,27 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
+import { CacheModule } from '@nestjs/cache-manager';
+import { Keyv } from 'keyv';
+import { KeyvCacheableMemory } from 'cacheable';
+import KeyvRedis from '@keyv/redis';
+import { ConfigModule } from '@nestjs/config';
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    CacheModule.registerAsync({
+      useFactory: () => {
+        return {
+          stores: [
+            new Keyv({
+              store: new KeyvCacheableMemory({ ttl: 60000, lruSize: 5000 }),
+            }),
+            new KeyvRedis('redis://localhost:6379'),
+          ],
+        };
+      },
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
